5c445017039252d6199dbc7292e1bb9e8476cd59
CyMain <okikiolacyrus@gmail.com>
2025-01-06 01:41:53 +0100

    JS, CSS progress
    Added functionality for numeric buttons(1) and operational buttons(+, =)

--- scripts/calc.js
+++ scripts/calc.js
@@ -5,9 +5,22 @@ function addToEqu(value){
     equ+=` ${value} `
   } else{
     equ+=`${value}`;
+    if(equ[equ.length - 3] == "+" || equ[equ.length - 3] == "*" || equ[equ.length - 3] == "/" || equ[equ.length - 3] == "-"){
+      document.querySelector('.output').innerHTML = eval(equ);
+    }
   }
-
+  console.log(equ);
   document.querySelector('.current-eq').innerHTML = equ;
 }
 
-alert("HEELOOOO!!!")
+function solveEqu(){
+  try {
+    document.querySelector('.output').innerHTML = eval(equ);
+    console.log(eval(equ)); 
+  } catch (error) {
+    document.querySelector('.output').innerHTML = 'please complete your expression.';
+    console.log('terrible equ~');
+  }
+  console.log(equ.length);
+  console.log(equ[3]);
+}
