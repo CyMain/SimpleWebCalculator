d4dc4e7dd19e53bd75f43778e7c2193d258eb83f
CyMain <okikiolacyrus@gmail.com>
2025-01-10 10:54:42 +0100

    Major changes in functionality.
    - Addition of direction buttons
    - Allowed continuations of equations after deriving an answer.
    -Adjusted proportions of calculator body and buttons

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,13 @@
-let equ = localStorage.getItem('equation')||` `;
+let equ =` `;
+
+//23 characters
 
 const buttons = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, '.', '/', '='];
+
+let leftScreenSideIndex= 0;
+let rightScreenSideIndex= 0;
+let lastScore = Number(localStorage.getItem('lastScore')) || 0;
+let isCalled= false;
 
 buttons.forEach((button)=>{
   if(button == '='){
@@ -25,17 +32,26 @@ buttons.forEach((button)=>{
 console.log(document.querySelector('.buttons').innerHTML);
 
 function addToEqu(value){
+  if(isCalled == true){
+    document.querySelector('.current-eq').innerHTML = `${lastScore}`;
+    equ = ` ${lastScore}`;
+    isCalled = false;
+  }
+
   if(value == '+'||value == '-'||value == '*'||value == '/'){
     equ+=` ${value} `;
   } else{
     equ+=`${value}`;
-    if(equ[equ.length - 3] == '+' || equ[equ.length - 3] == '*' || equ[equ.length - 3] == '/' || equ[equ.length - 3] == '-'){
+    if(eval(equ) !== ''){
       document.querySelector('.output').innerHTML = eval(equ);
       document.querySelector('.output').classList.add('transparent');
     }
   }
   console.log(equ);
-  document.querySelector('.current-eq').innerHTML = equ;
+  document.querySelector('.current-eq').innerHTML = equ.substring(equ.length-23, equ.length);
+  
+  leftScreenSideIndex = equ.length-23;
+  rightScreenSideIndex = equ.length;
 }
 
 function solveEqu(){
@@ -43,6 +59,9 @@ function solveEqu(){
     document.querySelector('.output').innerHTML = eval(equ);
     console.log(eval(equ)); 
     document.querySelector('.output').classList.remove('transparent');
+    localStorage.setItem('lastScore', eval(equ))
+    lastScore = eval(equ);
+    isCalled = true;
   } catch (error) {
     document.querySelector('.output').innerHTML = 'please complete your expression.';
     console.log('terrible equ~');
@@ -54,4 +73,17 @@ function clearEqu(){
   equ = ` `;
   document.querySelector('.current-eq').innerHTML = '';
   document.querySelector('.output').innerHTML = '';
+}
+
+function equScroll(dir){
+  if(dir == 'left' && leftScreenSideIndex > 0){
+    document.querySelector('.current-eq').innerHTML = equ.substring(leftScreenSideIndex-1, rightScreenSideIndex-1);
+    leftScreenSideIndex -= 1;
+    rightScreenSideIndex -= 1;
+  }
+  if(dir == 'right' && rightScreenSideIndex < equ.length){
+    document.querySelector('.current-eq').innerHTML = equ.substring(leftScreenSideIndex+1, rightScreenSideIndex+1);
+    leftScreenSideIndex += 1;
+    rightScreenSideIndex += 1;
+  }
 }
